74a30cedaae23ab3dfd64dfd10bc9eab07b135f6
Tilman Holube <tilman@holube.de>
2025-07-24 11:35:40 +0200

    Split document for readability and easier modification in the future (#15)
    * Split document for readability and easier modification in the future
    
    * Add server and move to /api/v1
    
    * Update version

---
+++ validate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+UID=$(id -u)
+GID=$(id -g)
+
+docker run --rm \
+  -u "${UID}":"${GID}" \
+  -v "${PWD}":/local openapitools/openapi-generator-cli:v7.13.0 validate \
+  -i /local/openapi.yaml
